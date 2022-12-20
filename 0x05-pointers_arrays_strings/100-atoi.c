ec225759d159dc3d644c1e7e412abd72463957b2
23-Jess <ireoguechej@gmail.com>
2022-12-19 19:41:16 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
  */
 int _atoi(char *s)
 {
-	int i, d, len, f, digit;
+	int i, d, n, len, f, digit;
 
 	i = 0;
 	d = 0;
@@ -21,7 +21,7 @@ int _atoi(char *s)
 
 	while (i < len && f == 0)
 	{
-		if (s[i] == '-;)
+		if (s[i] == '-')
 			++d;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
