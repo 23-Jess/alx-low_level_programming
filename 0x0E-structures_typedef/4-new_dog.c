ed7019bc46754d7e8a4b17e48e9d26d60bc6f065
23-Jess <ireoguechej@gmail.com>
2023-01-17 06:19:58 -0800

    second update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,10 +11,11 @@
 
 int _strlen(const char *str)
 {
-	int length = 0;
-	while (*str++)
-		length++;
-	return (length);
+int length = 0;
+
+while (*str++)
+length++;
+return (length);
 }
 
 /**
@@ -29,6 +30,7 @@ int _strlen(const char *str)
 char *_strcopy(char *dest, char *src)
 {
 	int i;
+
 	for (i = 0; src[i]; i++)
 		dest[i] = src[i];
 	dest[i] = '\0';
