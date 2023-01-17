1661522990e1f0c14be06eae728eb8ce946cf5b9
23-Jess <ireoguechej@gmail.com>
2023-01-17 06:06:50 -0800

    updated

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,14 +7,13 @@
  * @d: pointer to free
  *
  * Return: nothing
- * /
-
- void free_dog(dog_t *d)
+ */
+void free_dog(dog_t *d)
 {
-if (d)
-{
-free(d->name);
-free(d->owner);
-free(d);
-}
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
